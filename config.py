805b8f3b1d965d1065399ce11e19b902800bfba2
Artucuno1234 <Articuno1234@users.noreply.github.com>
2018-05-01 10:32:27 +0000

    [Arty] First Update!

---
+++ config.py
@@ -0,0 +1,11 @@
+# You can have 1 or more Prefixes!
+PREFIX = ["=", "=="]
+
+# The token is the thing that makes the bot run!
+token = 'Bot Token'
+
+# Syncs to Checks.py
+OWNERID = "ID"
+
+# Bump is Currently being worked on!
+bumpchannel = "Channel ID"
